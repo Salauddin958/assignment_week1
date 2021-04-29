9824468a3f398b408ac0f9f404839c6c3ddd613a
Mohammed Salauddin <Salauddinmd1312@gmail.com>
2021-04-29 13:44:06 +0530

    printing emojis with emojis package with go modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/emojis
+
+go 1.13
+
+require github.com/kyokomi/emoji v2.2.4+incompatible
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji"
+)
+
+func main() {
+	fmt.Println("Printing Emojis")
+	emoji.Println(":unicorn: unicorn!!!")
+	emoji.Println(":ox: ox!!!")
+	emoji.Println(":cow: cow!!!")
+}
