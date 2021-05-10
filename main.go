7ead77194eccc5c6ef4176401e4149c15dceaf64
Mohammed Salauddin <Salauddinmd1312@gmail.com>
2021-05-10 14:08:06 +0530

    random emojis

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/kyokomi/emoji"
 )
 
 func main() {
-	fmt.Println("Printing Emojis")
-	emoji.Println(":unicorn: unicorn!!!")
-	emoji.Println(":ox: ox!!!")
-	emoji.Println(":cow: cow!!!")
+	fmt.Println("Printing random Emojis")
+	emojis := map[int]string{
+		1: "baby",
+		2: "child",
+		3: "cow",
+		4: "girl",
+		5: "man",
+		6: "unicorn",
+		7: "ox",
+	}
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	for i := 0; i < 3; i++ {
+		key := r1.Intn(7) + 1
+		emoji.Println(":", emojis[key], ": ", emojis[key], "!!!")
+	}
 }
